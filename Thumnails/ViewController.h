fda04fbd5690447f45fdce69eacd511ddae7638f
Admin <admin@mac-admin.local>
2016-03-04 21:53:08 +0200

    Initial Commit

---
+++ Thumnails/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thumnails
+//
+//  Created by Admin on 3/4/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
